018e0a6de4f2ce082560bf4372c8d017b565f315
termslang <m.baynov@gmail.com>
2019-01-06 02:34:16 +0300

    fixes

--- src/demo/test.ml
+++ src/demo/test.ml
@@ -20,17 +20,18 @@ let verify_signed_message msg sign pk_sender =
   else
     Printf.printf "Invalid signature\n"
 
-let main = 
+let main1 = 
   let msg = "ECDSA demo!" in
   let signature = sign_message msg alice_sk in
     verify_signed_message msg signature alice_pk;
-
-(* let test2 =
- *   let bobs_secret = DH.create_session_key alice_pk bob_sk curve in
- *   let alices_secret = DH.create_session_key bob_pk alice_sk curve in
- *     (\* In a world without war alice and bob should share a secret *\)
- *     if (bobs_secret = alices_secret) then
- *       Printf.printf "Bob and alice shared a secret\n"
- *     else
- *       Printf.printf "We failed bob and alice\n" *)
-    
+;;
+let main2 =
+  let bobs_secret = DH.create_session_key alice_pk bob_sk curve in
+  let alices_secret = DH.create_session_key bob_pk alice_sk curve in
+    (* In a world without war alice and bob should share a secret *)
+    if (bobs_secret = alices_secret) then
+      Printf.printf "Bob and alice shared a secret\n"
+    else
+      Printf.printf "We failed bob and alice\n"
+;;
+let main = main1 ;; main2
